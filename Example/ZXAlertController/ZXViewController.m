98ba6196ac2e466bf0f122ae1c7b9a1b76a9dc96
iMac <285797382@qq.com>
2018-02-02 10:11:07 +0800

    v0.1.0okkk

--- Example/ZXAlertController/ZXViewController.m
+++ Example/ZXAlertController/ZXViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ZXViewController.h"
+#import "ZXAlertController.m"
 
 @interface ZXViewController ()
-
+@property (nonatomic, strong) ZXAlertController *alertVC;
 @end
 
 @implementation ZXViewController
@@ -18,6 +19,31 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    
+    for (int i =1; i<15; i++) {
+        ZXAlertAction *action =[ZXAlertAction actionWithTitle:@"好的" style:ZXAlertActionStyleDestructive handler:^(ZXAlertAction * _Nullable action) {
+            NSLog(@"可以的");
+        }];
+        [self.alertVC addAction:action];
+    }
+    
+    ZXAlertAction *action2 =[ZXAlertAction actionWithTitle:@"取消" style:ZXAlertActionStyleCancel handler:nil];
+    action2.enabled =NO;
+    
+    [_alertVC addAction:action2];
+    [_alertVC show];
+}
+
+-(ZXAlertController *)alertVC{
+    if (!_alertVC) {
+        _alertVC = [ZXAlertController alertControllerWithTitle:nil message:nil preferredStyle:ZXAlertControllerStyleActionSheet];
+        //        _alertVC.backgroundViewEffect = ZXBackgroundViewBlurEffect;
+    }
+    return _alertVC;
 }
 
 - (void)didReceiveMemoryWarning
